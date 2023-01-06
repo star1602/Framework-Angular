e92fb025c0501e138b6c0656a7113dbcd1dca254
Estrella <108537918+star1602@users.noreply.github.com>
2023-01-05 23:28:34 -0500

    S12

--- src/app/ejercicio.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EjercicioDirective } from './ejercicio.directive';
-
-describe('EjercicioDirective', () => {
-  it('should create an instance', () => {
-    const directive = new EjercicioDirective();
-    expect(directive).toBeTruthy();
-  });
-});
